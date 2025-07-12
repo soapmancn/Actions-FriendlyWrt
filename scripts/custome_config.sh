6b6a7820725a807a8f2ee12adb9bbeb8f08b1caf
soapman <33997593+soapmancn@users.noreply.github.com>
2025-07-12 15:38:47 +0800

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,3 +3,4 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+echo -e "shuangzizuo\nshuangzizuo" | passwd root
