c7328263ac7cac1e1a1011044b602ac5172cddaa
soapman <33997593+soapmancn@users.noreply.github.com>
2025-07-12 17:13:11 +0800

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,4 +3,4 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
-sed -i 's/^root:[^:]*/root:$1$ln.dVYPV$rclvHb1ieg3WTQLaqA/n01/' /etc/shadow
+sed -i 's/^root:[^:]*/root:$6$QpFzU9k9$g0wK1wLJb3zM4u0QFgVj3jHq4wWk8DgkQj6cYwK8Dh9YfHoF5j0tqN2w1qQjO6RzUe7WjI0vOeQqA9Vf4J2n/' /etc/shadow
